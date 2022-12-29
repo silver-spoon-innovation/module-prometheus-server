a5d14e01d89c26dd620e4ead2406c1a59ea182af
pamornpolh <pamornpol.hanmatheekuna@gmail.com>
2022-12-30 00:06:14 +0700

    extend helm timeout

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ resource "helm_release" "kube-prometheus-sssm" {
   chart            = "kube-prometheus-stack"
   namespace        = kubernetes_namespace.ns-monitoring.metadata.0.name
   create_namespace = false
+  timeout          = 1500 
 
   depends_on = [kubernetes_namespace.ns-monitoring]
 }
